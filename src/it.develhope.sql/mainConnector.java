cbd60975d5e14b1e17415b05a3bfb8f1e0023740
mircoForcella <forcella1996@gmail.com>
2023-04-03 00:44:19 +0200

    aggiornato per includere la query di creazione di una tabella e di inserimento dati.

--- src/it.develhope.sql/mainConnector.java
+++ src/it.develhope.sql/mainConnector.java
@@ -1,28 +1,53 @@
 package it.develhope.sql;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;;
+import java.sql.*;
+;
 
 public class mainConnector {
     public static void main(String[] args) {
 
 
         // db parameters
-        Connection conn = null;
+        Connection connection = null;
+        Statement statement = null;
+
         try{
         String url       = "jdbc:mysql://localhost:3306/newdb";
         String user      = "developer";
         String password  = "passwordhere";
 
         // create a connection to the database
-            conn = DriverManager.getConnection(url, user, password);
+            connection = DriverManager.getConnection(url, user, password);
+
+        // create students table
+            String createStudents = "CREATE TABLE students (" +
+                    "student_id INT(10) NOT NULL AUTO_INCREMENT," +
+                    "last_name VARCHAR(30)," +
+                    "first_name VARCHAR(30)," +
+                    "PRIMARY KEY (student_id))";
+            statement.executeUpdate(createStudents);
+
+        // populate students table
+            String insertQueryStudents = "INSERT INTO students (last_name, first_name) VALUES (?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(insertQueryStudents);
+            preparedStatement.setString(1,"Connor");
+            preparedStatement.setString(2,"John");
+            preparedStatement.executeUpdate();
+            preparedStatement.setString(1,"Connor");
+            preparedStatement.setString(2,"Sarah");
+            preparedStatement.executeUpdate();
+            preparedStatement.setString(1,"Schwarzenegger");
+            preparedStatement.setString(2,"Arnold");
+            preparedStatement.executeUpdate();
+            preparedStatement.setString(1,"Loken");
+            preparedStatement.setString(2,"Kristanna");
+
         } catch(SQLException e) {
             System.out.println(e.getMessage());
         } finally {
             try{
-                if(conn != null){
-                    conn.close();
+                if(connection != null){
+                    connection.close();
                 }
             }catch(SQLException ex){
                 System.out.println(ex.getMessage());
