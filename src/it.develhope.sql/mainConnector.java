8727bc7f2fa8b711bae6ad032db47cb14a763c51
mircoForcella <forcella1996@gmail.com>
2023-04-03 01:31:44 +0200

    aggiornato.

--- src/it.develhope.sql/mainConnector.java
+++ src/it.develhope.sql/mainConnector.java
@@ -1,24 +1,18 @@
 package it.develhope.sql;
 
 import java.sql.*;
-;
 
 public class mainConnector {
     public static void main(String[] args) {
 
-
-        // db parameters
         Connection connection = null;
-        Statement statement = null;
-
         try{
         String url       = "jdbc:mysql://localhost:3306/newdb";
         String user      = "developer";
         String password  = "passwordhere";
-
         // create a connection to the database
             connection = DriverManager.getConnection(url, user, password);
-
+            Statement statement = connection.createStatement();
         // create students table
             String createStudents = "CREATE TABLE students (" +
                     "student_id INT(10) NOT NULL AUTO_INCREMENT," +
@@ -26,7 +20,6 @@ public class mainConnector {
                     "first_name VARCHAR(30)," +
                     "PRIMARY KEY (student_id))";
             statement.executeUpdate(createStudents);
-
         // populate students table
             String insertQueryStudents = "INSERT INTO students (last_name, first_name) VALUES (?, ?)";
             PreparedStatement preparedStatement = connection.prepareStatement(insertQueryStudents);
@@ -41,6 +34,7 @@ public class mainConnector {
             preparedStatement.executeUpdate();
             preparedStatement.setString(1,"Loken");
             preparedStatement.setString(2,"Kristanna");
+            preparedStatement.executeUpdate();
 
         } catch(SQLException e) {
             System.out.println(e.getMessage());
